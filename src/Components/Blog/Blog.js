ef7924454dc5115ef7cbe1ed58bfeefe8bb6e9a1
Yash-R <rathorey467@gmail.com>
2021-12-03 15:22:12 +0530

    added blogs and projects

---
+++ src/Components/Blog/Blog.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+import { Row, Col, Card } from "antd";
+import { useLocation } from "react-router-dom";
+import style from "./Blog.module.css";
+import BlogCard from "../Cards/BlogCard";
+import { Blogs } from "./Blogs";
+import { ShareAltOutlined, HeartTwoTone } from "@ant-design/icons";
+
+export default function Blog() {
+  const [blogData, setBlogData] = useState({});
+
+  const location = useLocation();
+  useEffect(() => {
+    let check = location.pathname.split("/");
+    setBlogData(Blogs[check[3] - 1]);
+  }, [location]);
+
+  console.log("blogData", blogData);
+  return (
+    <Row justify="center" gutter={[0, 60]}>
+      <Col span={14}>
+        <Card
+          hoverable
+          cover={
+            <>
+              <div className={style.blogHeading}>
+                {blogData && blogData.title}
+              </div>
+              <img
+                className={style.blogImg}
+                alt="example"
+                src={blogData && blogData.url}
+              />
+            </>
+          }
+          actions={[
+            <div>
+              <ShareAltOutlined />
+              {"    "}Share
+            </div>,
+            <div>
+              <HeartTwoTone twoToneColor="#eb2f96" />
+              {"    "} Like
+            </div>,
+          ]}
+        >
+          <div className={style.container7text1}>
+            {blogData && blogData.blog}
+          </div>
+        </Card>
+      </Col>
+      <Col span={18}>
+        <Row justify="space-between" gutter={[30, 30]}>
+          <Col span={8}>
+            <BlogCard
+              id={1}
+              src="/assets/18_HomePage.png"
+              text="White Space Creating Ambiance with minimal Boxing & Louvers"
+              desc="Technology integrated Construction, Help us to provide you various facilities in single platform Technology integrated Construction, Help us to provide you various facilities in"
+              seen="108"
+              like="84"
+              time="1 Week"
+            />
+          </Col>
+          <Col span={8}>
+            <BlogCard
+              id={2}
+              src="/assets/18_HomePage.png"
+              text="White Space Creating Ambiance with minimal Boxing & Louvers"
+              desc="Technology integrated Construction, Help us to provide you various facilities in single platform Technology integrated Construction, Help us to provide you various facilities in"
+              seen="108"
+              like="84"
+              time="1 Week"
+            />
+          </Col>
+          <Col span={8}>
+            <BlogCard
+              id={3}
+              src="/assets/18_HomePage.png"
+              text="White Space Creating Ambiance with minimal Boxing & Louvers"
+              desc="Technology integrated Construction, Help us to provide you various facilities in single platform Technology integrated Construction, Help us to provide you various facilities in"
+              seen="108"
+              like="84"
+              time="1 Week"
+            />
+          </Col>
+        </Row>
+      </Col>
+      <Col span={24}></Col>
+    </Row>
+  );
+}
--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "antd";
 import style from "./Project.module.css";
 import FeatureCards from "../Cards/FeatureCards";
 import ImageGallery from "./ImageGallery";
 import AboutContractor from "./AboutContractor";
 import ProjectCard from "../Cards/ProjectCard";
-export default function index() {
+import { useLocation } from "react-router-dom";
+import { Projects } from "./Projects";
+
+export default function Project() {
+  const [projectData, setProjectData] = useState({});
+
+  const location = useLocation();
+  useEffect(() => {
+    let check = location.pathname.split("/");
+    setProjectData(Projects[check[2] - 1]);
+  }, [location]);
+
+  console.log(projectData);
   return (
     <Row justify="center" gutter={[0, 60]}>
       <Col span={18}>
         <img
           style={{ width: "100%" }}
           alt="project"
-          src="/assets/01_Project.png"
+          src={projectData && projectData.url}
         />
       </Col>
       <Col span={18}>
-        <div className={style.container1text1}>Mr. Rupesh Deshpande</div>
+        <div className={style.container1text1}>
+          {projectData && projectData.name}
+        </div>
         <FeatureCards
           sm="G + 1"
           rm="3,246"
@@ -29,20 +43,12 @@ export default function index() {
       <Col span={18}>
         <div className={style.container2text1}>
           Contractor -{" "}
-          <span className={style.container2text2}>L&R Construction</span>
+          <span className={style.container2text2}>
+            {projectData && projectData.contractorName}
+          </span>
         </div>
         <div className={style.container2text3}>
-          In the case of the Mauryan capital Pataliputra (near Patna), we have
-          Greek accounts, and that of Faxian; Megasthenes (a visitor around 300
-          BCE) mentions 564 towers and 64 gates in the city walls. Modern
-          excavations have uncovered a "massive palisade of teak beams held
-          together with iron dowels".[7] A huge apadana-like hall with eighty
-          sandstone columns shows clear influence from contemporary Achaemenid
-          Persia.[8] The single massive sandstone Pataliputra capital shows
-          clear Hellenistic features, reaching India via Persia.[9] The famous
-          Ashoka columns show great sophistication, and a variety of influences
-          in their details. In both these cases a now-vanished Indian
-          predecessor tradition in wood is likely.[10]
+          {projectData && projectData.constructorData}
         </div>
       </Col>
       <Col span={18}>
@@ -50,7 +56,11 @@ export default function index() {
         <ImageGallery />
       </Col>
       <Col span={18}>
-        <AboutContractor />
+        <AboutContractor
+          name={projectData && projectData.name}
+          about={projectData && projectData.about}
+          url={projectData && projectData.videourl}
+        />
       </Col>
       <Col span={18}>
         <div className={style.container1text1}>Similar Projects</div>
@@ -61,7 +71,7 @@ export default function index() {
         >
           <Col span={6} xs={24} sm={12} md={8} lg={6}>
             <ProjectCard
-              id="17"
+              id="1"
               src="/assets/05project.png"
               name="Mr. Mahesh Sahu"
               add="Lalghati, Bhopal"
@@ -72,7 +82,7 @@ export default function index() {
           </Col>
           <Col span={6} xs={24} sm={12} md={8} lg={6}>
             <ProjectCard
-              id="18"
+              id="2"
               src="/assets/07project.png"
               name="Grace Garden Housing"
               add="BHEL, Bhopal"
@@ -83,7 +93,7 @@ export default function index() {
           </Col>
           <Col span={6} xs={24} sm={12} md={8} lg={6}>
             <ProjectCard
-              id="19"
+              id="3"
               src="/assets/04project.png"
               name="Mr & Mrs Sahu"
               add="Nayapura, Bhopal"
@@ -94,7 +104,7 @@ export default function index() {
           </Col>
           <Col span={6} xs={24} sm={12} md={8} lg={6}>
             <ProjectCard
-              id="20"
+              id="4"
               src="/assets/09project.png"
               name="Mr. Geeta Residence"
               add="Lords Colony, Lalghati, Bhopal"
