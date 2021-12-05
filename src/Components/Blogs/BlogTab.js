561191c1d4d74672a3e5ed13ad377d9d44ed2c61
Anshul-Ga <anshul.gautam2021@gmail.com>
2021-12-05 11:48:56 +0530

    Edit blog

--- src/Components/Blogs/BlogTab.js
+++ src/Components/Blogs/BlogTab.js
@@ -109,7 +109,21 @@ export default function BlogTab() {
         Comming Soon !
       </TabPane>
       <TabPane tab="Construction" key="3">
-        Comming Soon !
+      <div style={{ paddingTop: "20px", paddingBottom: "20px" }}>
+          <Row justify="center" gutter={[20, 20]}>
+            <Col sm={12} md={8} lg={8} xl={6} xxl={6}>
+              <BlogCard
+                id={9}
+                src="/assets/18_HomePage.png"
+                text="White Space Creating Ambiance with minimal Boxing & Louvers"
+                desc="Technology integrated Construction, Help us to provide you various facilities in single platform Technology integrated Construction, Help us to provide you various facilities in"
+                seen="108"
+                like="84"
+                time="1 Week"
+              />
+            </Col>
+            </Row>
+        </div>
       </TabPane>
       <TabPane tab="Industry" key="4">
         Comming Soon !
